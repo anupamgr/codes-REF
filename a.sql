b69405eed2cbbd9cd190c9040ec6a2bcab58822c
anupamgr <anupamgarainkashyap@gmail.com>
2019-08-19 15:58:21 +0530

    Delete a.sql

--- a.sql
+++
@@ -1,4 +0,0 @@
-
-insert into `lu_field` ( `field_name`, `display_name`, `data_type_id`, `field_type_id`, `field_level_id`, `field_display_type_id`, `column_name`, `is_editable`, `non_editable`, `active`, `fk_account_id`, `description`, `is_indexed`, `is_help_enabled`, `fk_field_ml_lang_type_id`, `fk_field_group_id`, `hql_field_hierarchy`, `source_entity`, `sortable`) 
-('acctClerk','acctClerk','62','1','10','1',NULL,'1','0','1','-1',NULL,'0','0','1',NULL,NULL,NULL,'1'),
-('acctClerkCode','acctClerkCode','62','1','10','1',NULL,'1','0','1','-1',NULL,'0','0','1',NULL,NULL,NULL,'1');
